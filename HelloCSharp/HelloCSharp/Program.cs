5d946d527ccf517f20a0471f6a6490f0b63d230c
Emil <Emil@EMIL-PC>
2016-11-21 23:54:41 +0800

    Upload HelloCSharp

---
+++ HelloCSharp/HelloCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good good study!");
+            Console.WriteLine("Day day up!");
+
+            Console.ReadKey(true);
+        }
+    }
+}
